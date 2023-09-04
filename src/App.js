980d3bf31f823fe3bb1f8b9329e7a2a896c31730
wxodi0 <taeyang127.0.0.1@gmail.com>
2023-09-04 12:06:04 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React , {useEffect, useState} from 'react';
+import Viewer from './Components/Viewer';
+import Controller from './Components/Controller';
 import './App.css';
 
 function App() {
+  //선언
+  const [count , setCount] = useState(0);
+  const [text , setText] = useState("gkdl");
+  //값 증가
+  const handleSetCount = (value) => {
+    setCount(count + value);
+  }
+
+  const handleChangeText = (e) => {
+      setText(e.target.value);
+  }
+
+  useEffect(() => {
+    console.log("업데이트 : ",count , text);
+  },[count,text]);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Simple Counter</h1>
+      <section>
+        <input value={text} onChange={handleChangeText}></input>
+      </section>
+      <section>
+        <Viewer count={count} />
+      </section>
+      <section>
+        <Controller handleSetCount={handleSetCount}/>
+      </section>
     </div>
   );
 }
